98afd0fdd36d56b6403aa0721802d5e3ed7ff910
expresslineto <coffeeloversgithub@gmail.com>
2020-08-17 21:16:13 -0700

    add new file

---
+++ src/image/image.js
@@ -0,0 +1,39 @@
+
+import page1 from "./tully/final2020.jpg";
+import page2 from "./tully/final20202.jpg";
+import page3 from "./tully/final20203.jpg";
+import page4 from "./tully/final20204.jpg";
+import page5 from "./tully/final20205.jpg";
+import page6 from "./tully/final20206.jpg";
+import page7 from "./tully/final20207.jpg";
+const listMenu = [
+  {
+    page: "1",
+    itemsImage: page1,
+  },
+  {
+    page: "2",
+    itemsImage: page2,
+  },
+  {
+    page: "3",
+    itemsImage: page3,
+  },
+  {
+    page: "4",
+    itemsImage: page4,
+  },
+  {
+    page: "5",
+    itemsImage: page5,
+  },
+  {
+    page: "6",
+    itemsImage: page6,
+  },
+  {
+    page: "7",
+    itemsImage: page7,
+  },
+];
+export default listMenu;
