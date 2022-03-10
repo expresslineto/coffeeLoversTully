8dde37c5d6287f099c17f49ed251a2d221e0d810
Henry Hieu Nguyen <hieunugent@gmail.com>
2022-03-09 23:27:18 -0800

    update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import React from 'react';
+import React from "react";
 
-import './App.css';
-import MenuForm from './menu';
+import "./App.css";
+import MenuForm from "./menu";
 import list from "./image/image";
 
-import logo from "./image/tully/logo/logo.png"
+import logo from "./image/tully/logo/logo.png";
 
 import { makeStyles } from "@material-ui/core/styles";
-import { Paper, Grid } from '@material-ui/core';
-
+import { Paper, Grid } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
-  img: {
+  imgi: {
     margin: 0,
     display: "flex",
 
@@ -33,25 +32,20 @@ function App() {
       <Paper>
         <Grid container>
           <Grid xs={4}>
-            <figure className={classes.img}>
-              <img
-                className={classes.img}
-                alt="complex"
-                src={logo}
-              />
-            </figure>
-          </Grid>
+            <figure className={classes.imgi}>
+              <img className={classes.imgi} alt="complex" src={logo} />{" "}
+            </figure>{" "}
+          </Grid>{" "}
           <Grid xs={8} className={classes.storeInfo}>
-            <h4 > 1696 Tully Rd, San Jose, CA 95122</h4>
-            <h4 > (408) 784-3379</h4>
-          </Grid></Grid>
+            <h4> 1696 Tully Rd, San Jose, CA 95122 </h4>{" "}
+            <h4> (408) 784 - 3379 </h4>{" "}
+          </Grid>
+        </Grid>
       </Paper>
-      
-      {list.map((item, index) => {
-          return <MenuForm id={index} itemsImage={item.itemsImage} />;
 
+      {list.map((item, index) => {
+        return <MenuForm id={index} itemsImage={item.itemsImage} />;
       })}
-     
     </div>
   );
 }
--- src/menu.js
+++ src/menu.js
@@ -1,35 +1,27 @@
-import React from 'react'
+import React from "react";
 import Paper from "@material-ui/core/Paper";
 import { makeStyles } from "@material-ui/core/styles";
-
 
 const useStyles = makeStyles((theme) => ({
   img: {
     margin: 0,
     display: "flex",
-    
-    maxWidth: "100%",
-    
-    backgroundColor: "black",
     alignItems: "center",
+    backgroundColor: "black",
+    minWidth: "100vw",
   },
 }));
 
-
 export default function menu(props) {
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    const classes = useStyles();
-    return (
-      <div>
-        <Paper>
-          <figure className={classes.img}>
-            <img
-              className={classes.img}
-              alt="complex"
-              src={props.itemsImage}
-            />
-          </figure>
-        </Paper>
-      </div>
-    );
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const classes = useStyles();
+  return (
+    <div>
+      <Paper>
+        <figure className={classes.img}>
+          <img className={classes.img} alt="complex" src={props.itemsImage} />
+        </figure>
+      </Paper>
+    </div>
+  );
 }
