b1f7f9bcc4282f267d3424e1239263a595df4150
hieunugent <hieunugent@gmail.com>
2022-07-25 11:52:36 -0700

    update

--- src/image/image.js
+++ src/image/image.js
@@ -1,21 +1,20 @@
-import page1 from "./tully/lover tully 03092922.jpg";
-import page2 from "./tully/lover tully 030929223.jpg";
-import page3 from "./tully/lover tully 030929224.jpg";
-import page4 from "./tully/lover tully 030929228.jpg";
-import page5 from "./tully/lover tully 030929229.jpg";
-import page6 from "./tully/lover tully 0309292210.jpg";
-import page7 from "./tully/lover tully 0309292211.jpg";
-import page8 from "./tully/lover tully 0309292212.jpg";
-import page9 from "./tully/lover tully 0309292213.jpg";
-import page10 from "./tully/lover tully 0309292214.jpg";
-import page11 from "./tully/lover tully 0309292215.jpg";
-import page12 from "./tully/lover tully 0309292216.jpg";
-import page13 from "./tully/lover tully 0309292217.jpg";
-import page14 from "./tully/lover tully 0309292218.jpg";
-import page15 from "./tully/lover tully 0309292219.jpg";
-import page16 from "./tully/lover tully 0309292220.jpg";
-import page17 from "./tully/lover tully 0309292221.jpg";
-import page18 from "./tully/lover tully 0309292222.jpg";
+import page1 from "./tully/lover tully 20220121.jpg";
+import page2 from "./tully/lover tully 20220724.jpg";
+import page3 from "./tully/lover tully 202207243.jpg";
+import page4 from "./tully/lover tully 202207244.jpg";
+import page5 from "./tully/lover tully 202207245.jpg";
+import page6 from "./tully/lover tully 202207246.jpg";
+
+// import page8 from "./tully/lover tully 0309292212.jpg";
+// import page9 from "./tully/lover tully 0309292213.jpg";
+// import page10 from "./tully/lover tully 0309292214.jpg";
+// import page11 from "./tully/lover tully 0309292215.jpg";
+// import page12 from "./tully/lover tully 0309292216.jpg";
+// import page13 from "./tully/lover tully 0309292217.jpg";
+// import page14 from "./tully/lover tully 0309292218.jpg";
+// import page15 from "./tully/lover tully 0309292219.jpg";
+// import page16 from "./tully/lover tully 0309292220.jpg";
+
 const listMenu = [{
         page: "1",
         itemsImage: page1,
@@ -40,21 +39,20 @@ const listMenu = [{
         page: "6",
         itemsImage: page6,
     },
-    {
-        page: "7",
-        itemsImage: page7,
-    },
-    { page: "8", itemsImage: page8 },
-    { page: "9", itemsImage: page9 },
-    { page: "10", itemsImage: page10 },
-    { page: "11", itemsImage: page11 },
-    { page: "12", itemsImage: page12 },
-    { page: "13", itemsImage: page13 },
-    { page: "14", itemsImage: page14 },
-    { page: "15", itemsImage: page15 },
-    { page: "16", itemsImage: page16 },
-    { page: "17", itemsImage: page17 },
-    { page: "18", itemsImage: page18 },
+    // {
+    //     page: "7",
+    //     itemsImage: page7,
+    // // },
+    // { page: "8", itemsImage: page8 },
+    // { page: "9", itemsImage: page9 },
+    // { page: "10", itemsImage: page10 },
+    // { page: "11", itemsImage: page11 },
+    // { page: "12", itemsImage: page12 },
+    // { page: "13", itemsImage: page13 },
+    // { page: "14", itemsImage: page14 },
+    // { page: "15", itemsImage: page15 },
+    // { page: "16", itemsImage: page16 },
+   
 
 ];
 
