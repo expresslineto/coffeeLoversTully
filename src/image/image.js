8f6a184a7caf5086e7c209ce8e7d3818baac1b22
hieunugent <hieunugent@gmail.com>
2024-07-04 14:50:07 -0700

    update 07042024

--- src/image/image.js
+++ src/image/image.js
@@ -2,22 +2,23 @@ import page1 from "./tully/1-01 low.jpg";
 import page2 from "./tully/2-01 low.jpg";
 import page3 from "./tully/3-01 low.jpg";
 import page4 from "./tully/4-01 low.jpg";
+
 import page5 from "./tully/lover aborn 2023-03-0310.jpg";
 import page6 from "./tully/lover aborn 2023-03-0311.jpg";
-
-import page8 from "./tully/lover aborn 2023-03-0312.jpg";
+// import page8 from "./tully/lover aborn 2023-03-0312.jpg";
 import page9 from "./tully/lover aborn 2023-03-0313.jpg";
-import page10 from "./tully/lover aborn 2023-03-0314.jpg";
+// import page10 from "./tully/lover aborn 2023-03-0314.jpg";
 import page11 from "./tully/lover aborn 2023-03-036.jpg";
 import page12 from "./tully/lover aborn 2023-03-037.jpg";
 import page13 from "./tully/lover aborn 2023-03-038.jpg";
 import page14 from "./tully/lover aborn 2023-03-039.jpg";
-import page15 from "./tully/lover tully 2024-02-05.jpg";
-import page16 from "./tully/lover tully 2024-02-052.jpg";
-import page17 from "./tully/lover tully 2024-02-053.jpg";
-import page18 from "./tully/lover tully 2024-02-054.jpg";
-import page19 from "./tully/lover tully 2024-02-055.jpg";
-import page20 from "./tully/lover tully 2024-02-056.jpg"
+
+import page15 from "./tully/lover tully 2024-07-01.jpg";
+import page16 from "./tully/lover tully 2024-07-012.jpg";
+import page17 from "./tully/lover tully 2024-07-013.jpg";
+import page18 from "./tully/lover tully 2024-07-014.jpg";
+import page19 from "./tully/lover tully 2024-07-015.jpg";
+import page20 from "./tully/lover tully 2024-07-016.jpg"
 
 const listMenu = [{
         page: "1",
@@ -46,9 +47,9 @@ const listMenu = [{
     // {   page: "7",
     //     itemsImage: page7,
     // },
-    { page: "8", itemsImage: page8 },
+    // { page: "8", itemsImage: page8 },
     { page: "9", itemsImage: page9 },
-    { page: "10", itemsImage: page10 },
+    // { page: "10", itemsImage: page10 },
     { page: "11", itemsImage: page11 },
     { page: "12", itemsImage: page12 },
     { page: "13", itemsImage: page13 },
