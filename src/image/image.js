740b8d723a6d26830b602de11ee088dd801ee523
hieunugent <hieunugent@gmail.com>
2025-05-12 22:20:42 +0700

    update

--- src/image/image.js
+++ src/image/image.js
@@ -1,24 +1,24 @@
-import page1 from "./tully/1-01 low.jpg";
-import page2 from "./tully/2-01 low.jpg";
-import page3 from "./tully/3-01 low.jpg";
-import page4 from "./tully/4-01 low.jpg";
+import page1 from "./tully/lover tully 2025-05 v2.jpg";
+import page2 from "./tully/lover tully 2025-05 v22.jpg";
+import page3 from "./tully/lover tully 2025-05 v23.jpg";
+import page4 from "./tully/lover tully 2025-05 v24.jpg";
 
-import page5 from "./tully/lover aborn 2023-03-0310.jpg";
-import page6 from "./tully/lover aborn 2023-03-0311.jpg";
+import page5 from "./tully/lover tully 2025-05 v26.jpg";
+import page6 from "./tully/lover tully 2025-05.jpg";
 // import page8 from "./tully/lover aborn 2023-03-0312.jpg";
-import page9 from "./tully/lover aborn 2023-03-0313.jpg";
+import page9 from "./tully/lover tully 2025-052.jpg";
 // import page10 from "./tully/lover aborn 2023-03-0314.jpg";
-import page11 from "./tully/lover aborn 2023-03-036.jpg";
-import page12 from "./tully/lover aborn 2023-03-037.jpg";
-import page13 from "./tully/lover aborn 2023-03-038.jpg";
-import page14 from "./tully/lover aborn 2023-03-039.jpg";
+import page11 from "./tully/lover tully 2025-053.jpg";
+import page12 from "./tully/lover tully 2025-054.jpg";
+import page13 from "./tully/lover tully 2025-055.jpg";
+import page14 from "./tully/lover tully 2025-0512.jpg";
 
-import page15 from "./tully/lover tully 2024-07-01.jpg";
-import page16 from "./tully/lover tully 2024-07-012.jpg";
-import page17 from "./tully/lover tully 2024-07-013.jpg";
-import page18 from "./tully/lover tully 2024-07-014.jpg";
-import page19 from "./tully/lover tully 2024-07-015.jpg";
-import page20 from "./tully/lover tully 2024-07-016.jpg"
+import page15 from "./tully/lover tully 2025-0513.jpg";
+import page16 from "./tully/lover tully 2025-0514.jpg";
+import page17 from "./tully/lover tully 2025-0515.jpg";
+// import page18 from "./tully/lover tully 2024-07-014.jpg";
+// import page19 from "./tully/lover tully 2024-07-015.jpg";
+// import page20 from "./tully/lover tully 2024-07-016.jpg"
 
 const listMenu = [{
         page: "1",
@@ -59,9 +59,9 @@ const listMenu = [{
     { page: "16", itemsImage: page16 },
 
     { page: "17", itemsImage: page17 },
-    { page: "18", itemsImage: page18 },
-    { page: "19", itemsImage: page19 },
-    { page: "20", itemsImage: page20 },
+    // { page: "18", itemsImage: page18 },
+    // { page: "19", itemsImage: page19 },
+    // { page: "20", itemsImage: page20 },
 
 
 
